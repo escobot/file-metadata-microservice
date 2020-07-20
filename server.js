6dfde20e01ccdeda095931ad9b067a3f6321d782
Pablo Gonzalez <pabgonza1995@gmail.com>
2020-07-20 11:32:16 -0400

    finish file metadata microservice

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const multer  = require('multer')
+let upload = multer({ dest: 'uploads/' })
+
+app.get('/*', function (req, res) {
+  res.send("<form action=\"/upload\" method=\"post\" enctype=\"multipart/form-data\"><input type=\"file\" name=\"avatar\" /><input type=\"submit\" value=\"Submit\"></form>");
+})
+
+app.post('/upload', upload.single('avatar'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    res.send({'size': req.file.size})
+  })
+
+app.listen(3000);
+console.log('App running on http://localhost:3000');
